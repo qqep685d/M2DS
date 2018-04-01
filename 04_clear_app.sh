77d59480b7b1227e86685ac609235dfaae832453
Atsushi Ohta <qqep685d@gmail.com>
2018-04-01 14:16:03 +0900

    fix cmd

--- 04_clear_app.sh
+++ 04_clear_app.sh
@@ -1,5 +1,4 @@
 cd m2ds/
-docker run --rm -v "$PWD":/home -w /home -p 8000:8000 m2ds_app \
-rm -f db.sqlite3 \
-&& rm -f mm/static/downloaded_files/* \
-&& rm -f mm/static/uploaded_files/*
+docker run --rm -v "$PWD":/home -w /home m2ds_app rm -f db.sqlite3
+docker run --rm -v "$PWD":/home -w /home m2ds_app rm -f mm/static/downloaded_files/*
+docker run --rm -v "$PWD":/home -w /home m2ds_app rm -f mm/static/uploaded_files/*
