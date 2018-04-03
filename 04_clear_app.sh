48b96ceeb0d267707a02b25a13058017688293ad
qqep685d <qqep685d@gmail.com>
2018-04-03 11:10:23 +0900

    fix 04_clear_app.sh

--- 04_clear_app.sh
+++ 04_clear_app.sh
@@ -1,5 +1,13 @@
 cd m2ds/
-docker run --rm -v "$PWD":/home -w /home m2ds rm -rf db.sqlite3
-docker run --rm -v "$PWD":/home -w /home m2ds rm -rf mm/migrations/*
-docker run --rm -v "$PWD":/home -w /home m2ds rm -rf mm/static/download_files/*
-docker run --rm -v "$PWD":/home -w /home m2ds rm -rf mm/static/upload_files/*
+# delete db.sqlite3
+# delete files in mm/migrations/
+# delete files in mm/static/download_files/
+# delete files in mm/static/upload_files/
+docker run --rm -v "$PWD":/home -w /home m2ds \
+    rm -rf db.sqlite3 \
+        mm/__pycache__ \
+        mm/migrations/* \
+        mm/static/download_files/* \
+        mm/static/upload_files/*
+# copy mm/__init__.py to mm/migrations/
+docker run --rm -v "$PWD":/home -w /home m2ds cp mm/__init__.py mm/migrations/
--- m2ds/m2ds/settings.py
+++ m2ds/m2ds/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 #LANGUAGE_CODE = 'en-us'
-#LANGUAGE_CODE = 'ja'
+LANGUAGE_CODE = 'ja'
 
 #TIME_ZONE = 'UTC'
 TIME_ZONE = 'Asia/Tokyo'
